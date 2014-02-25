ccebc6afbedf85bf52e077edb4de9ce3e15e1afd
Alisa Prude-Hunt <user@Users-Computer-5.local>
2014-02-25 10:38:51 -0500

    My first attempt at a platform game in Unity

---
+++ Assets/lilguy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lilguy : MonoBehaviour {
+	
+	float speed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Transform player = GetComponent<Transform>();
+		float deltaPosition = speed * Time.deltaTime;
+		
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			player.Translate(-deltaPosition, 0, 0);
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			player.Translate(deltaPosition, 0, 0);
+		}
+		
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			player.Translate(0, 0, -deltaPosition);
+		}
+		
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			player.Translate(0, 0, deltaPosition);
+		}
+		
+	}
+}
